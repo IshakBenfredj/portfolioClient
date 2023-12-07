28a9f56b60c832056a29445e29a5ace1afc90821
Ishak Benfredj <ishakbanfradje@gmail.com>
2023-12-07 17:49:07 +0100

    changes

--- app/sections/Landing.jsx
+++ app/sections/Landing.jsx
@@ -5,7 +5,7 @@ export default function Landing() {
     <div className="bg-[#fafafa] pt-[52px] relative overflow-hidden bg-gradient-to-r dark:from-gray-800 dark:to-gray-950 w-full h-screen">
       <img
         src="/landingBg.png"
-        className="object-cover absolute z-10 right-0 top-[52px] h-[calc(100vh - 52px)] h-full select-none"
+        className="object-cover absolute z-10 right-0 top-[52px] w-2/3 md:w-fit md:h-full h-1/2 select-none"
         alt=""
       />
       <div className="container py-20 text-center relative z-30">
